41f398372840e0139103c7059a7d28388a0af82c
jfogelmanpyramid <jfogelman@pyramidsystems.com>
2020-09-18 10:40:26 -0400

    Updates

---
+++ precache-manifest.7b67ad62dbf631137aefc325a7440e09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf3749515035f152eab225982f024bcb",
+    "url": "/screenerTest/index.html"
+  },
+  {
+    "revision": "ab00a35edde19001a908",
+    "url": "/screenerTest/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "340aac5933c756125f93",
+    "url": "/screenerTest/static/js/2.a0c31166.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/screenerTest/static/js/2.a0c31166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab00a35edde19001a908",
+    "url": "/screenerTest/static/js/main.4f8df828.chunk.js"
+  },
+  {
+    "revision": "8b4bc5f04721990ee18c",
+    "url": "/screenerTest/static/js/runtime-main.064a7913.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/screenerTest/static/media/logo.5d5d9eef.svg"
+  }
+]);
